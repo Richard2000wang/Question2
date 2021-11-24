2b63b56eb6d35db89d6482f55734abac85bd409f
Richard2000wang <richard2000wang@outlook.com>
2021-11-23 16:14:21 -0800

    first commit

---
+++ IOhandler.js
@@ -0,0 +1,47 @@
+/*
+ * Project:
+ * File Name: IOhandler.js
+ * Description: Collection of functions for files input/output related operations
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const unzipper = require("unzipper"),
+  fs = require("fs"),
+  PNG = require("pngjs").PNG,
+  path = require("path");
+
+/**
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {promise}
+ */
+const unzip = (pathIn, pathOut) => {};
+
+/**
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
+ * @return {promise}
+ */
+const readDir = (dir) => {}; // Make sure it only returns png files 
+
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+const grayScale = (pathIn, pathOut) => {}; //pathIn Name of the image pathOut is the new name of the image after blackout
+
+module.exports = {
+  unzip,
+  readDir,
+  grayScale,
+};
